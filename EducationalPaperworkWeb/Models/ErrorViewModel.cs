689171a641e796e14ba5fbba84a50bd8cc5f1db6
Vladislav <vladmemasik@gmail.com>
2024-02-29 19:08:02 +0200

    Добавьте файлы проекта.

---
+++ EducationalPaperworkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationalPaperworkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
