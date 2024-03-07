ae0f07543fa60dfc7cddbf2dbcc8e89479555a36
Vladislav <vladmemasik@gmail.com>
2024-03-07 17:40:36 +0200

    css don't work

--- EducationalPaperworkWeb.Domain/Domain/Models/User/UserLogIn.cs
+++ EducationalPaperworkWeb.Domain/Domain/Models/User/UserLogIn.cs
@@ -4,7 +4,12 @@ namespace EducationalPaperworkWeb.Domain.Domain.Models.User
 {
     public class UserLogIn
     {
+        [Required(ErrorMessage = "Пусте поле!")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некоректна адреса пошти!")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Пусте поле!")]
+        [MinLength(8, ErrorMessage = "Мінімальна довжина поля 8 символів!")]
+        [MaxLength(50, ErrorMessage = "Максимальна довжина поля 50 символів!")]
         public string Password { get; set; }
     }
 }
